eaa73eaa4b42b66b30f281999847e240e6991659
Vitor <vitor.rosa995@hotmail.com>
2020-06-24 13:07:50 -0300

    meu app python

---
+++ meuapp.py
@@ -0,0 +1,36 @@
+# mes1 = int(input('Valor da venda no mês de janeiro \n'))
+# mes2 = int(input('Valor a venda no mês de fevereiro\n'))
+# mes3 = int(input('Valor da venda no Mês de março\n'))
+# mes4 = int(input('Valor da venda no Mês de abril\n'))
+#
+# media = (mes1+mes2+mes3+mes4)/4
+#
+# if media >= 5000:
+#     print('Vendedor recebe abono de 10%. R${}\n'.format((10*1050)/100))
+# if media < 5000:
+#     print('Vendedor recebe abono de 3%. R${}\n'.format((3*1050)/100))
+
+# lista = [3, 5, 7, 10, 11]
+# resultado = []
+# for x in lista:
+#     if x % 2 == 1:
+#         resultado.append(x)
+# print(resultado)
+
+mariana = 2 # dona da casa
+renato = 4
+larissa = 2
+rafael = 5
+augusto = 1
+rafaela = 3
+
+dedos = {mariana, renato, larissa, rafael, augusto, rafaela}
+participantes =  #quantidade de participantes
+somaDedos =  #soma dos valores de cada dedo
+dedoapontadopara = 0
+for x in range(somaDedos):
+  if dedoapontadopara > participantes:
+     dedoapontadopara = 0
+  dedoapontadopara += 1
+dedos = _______ #converter dedos para arquivo tipo 'list'
+print('No final o dedo foi apontado para {}.'.format(dedos[dedoapontadopara]))
